ab03e8a7dc6616122d0e81b6bc393cdf50d8d746
Trestan MERVIN <trestan.mervin@gmail.com>
2019-05-20 14:29:30 +0200

    Added gitignore + implemented Regex

--- src/main/scala/ComputorV1/ComputorV1.scala
+++ src/main/scala/ComputorV1/ComputorV1.scala
@@ -1,17 +1,32 @@
 package ComputorV1
-import io
+import scala.io
+import scala.util.matching.Regex
 
 object ComputorV1 {
 
 
+  def splitEquation(input: String): List[String] = {
+    input.split("=").toList
+  }
 
   def removeAllWhiteSpaces(input: String): String = {
     input.replaceAll("\\s", "")
   }
 
   def main(args: Array[String]): Unit = {
-    println("Type an equation ")
-    val input = scala.io.StdIn.readLine()
-    println("Did you type this ? " + removeAllWhiteSpaces(input))
+
+    val regTest = new Regex("([-+]?(?:\\d+\\.)?\\d+)(?:[\\*]?[Xx][\\^]?(\\d+))?")
+    val eqTest = removeAllWhiteSpaces("5 * X^0 + 4 * X^1 - 9.3 * X^2 + 5 = 1 * X^0")
+    val matchTest = regTest.findAllIn(eqTest).map {
+      case regTest(a, b) => (a, b)
+    }.toList
+
+    println(matchTest)
+
+//    println("Type an equation ")
+//    "hello world".split(" ").foreach(println)
+//    val input = scala.io.StdIn.readLine()
+//    val returnValue = splitEquation(removeAllWhiteSpaces(input))
+//    returnValue.foreach(println)
   }
 }
