94f4105a3b1fe835e4ece6ea5b22302e65f967cd
Trestan MERVIN <trestan.mervin@gmail.com>
2019-05-20 11:11:46 +0200

    Adding project files

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ComputorV1"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
--- src/main/scala/ComputorV1/ComputorV1.scala
+++ src/main/scala/ComputorV1/ComputorV1.scala
@@ -1,5 +1,17 @@
 package ComputorV1
+import io
 
 object ComputorV1 {
 
+
+
+  def removeAllWhiteSpaces(input: String): String = {
+    input.replaceAll("\\s", "")
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Type an equation ")
+    val input = scala.io.StdIn.readLine()
+    println("Did you type this ? " + removeAllWhiteSpaces(input))
+  }
 }
