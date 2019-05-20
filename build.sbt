cd3b80b3731e98b184bc221654db7e7018c559c5
Trestan MERVIN <trestan.mervin@gmail.com>
2019-05-20 17:07:59 +0200

    Parser almost finished. Float/Int parsing to be improved

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "ComputorV1"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+scalacOptions += "-Ypartial-unification"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0-M1"
--- src/main/scala/ComputorV1/ComputorV1.scala
+++ src/main/scala/ComputorV1/ComputorV1.scala
@@ -1,27 +1,54 @@
 package ComputorV1
 import scala.io
+import scala.util.Try
 import scala.util.matching.Regex
+import cats.implicits._
 
 object ComputorV1 {
 
+  type coefAndExp = Map[Option[Int], Int]
 
-  def splitEquation(input: String): List[String] = {
-    input.split("=").toList
+
+  def splitEquation(s: String): List[String] = s.split("=").toList
+
+  def tryToInt(s: String): Option[Int] = Try(s.toInt).toOption
+
+  def removeAllWhiteSpaces(s: String): String = s.replaceAll("\\s", "")
+
+  def parseCoef(s: String): coefAndExp = {
+    val r = new Regex("([-+]?(?:\\d+\\.)?\\d+)(?:[\\*]?[Xx][\\^]?(\\d+))?")
+    val sWithoutSpaces = removeAllWhiteSpaces(s)
+
+//    val eqTestLeftRight = splitEquation().map(a => println(a.toString))
+    r.findAllIn(sWithoutSpaces).map {
+      case r(a, null) => (None, a.toInt)
+      case r(a, b) => (Option(b.toInt), a.toInt)
+    }.toMap
   }
 
-  def removeAllWhiteSpaces(input: String): String = {
-    input.replaceAll("\\s", "")
+  def invertMap(m: coefAndExp): coefAndExp = {
+    m map {
+      case (exp, value) => (exp, -value)
+    }
   }
 
   def main(args: Array[String]): Unit = {
 
-    val regTest = new Regex("([-+]?(?:\\d+\\.)?\\d+)(?:[\\*]?[Xx][\\^]?(\\d+))?")
-    val eqTest = removeAllWhiteSpaces("5 * X^0 + 4 * X^1 - 9.3 * X^2 + 5 = 1 * X^0")
-    val matchTest = regTest.findAllIn(eqTest).map {
-      case regTest(a, b) => (a, b)
-    }.toList
+    val eqTest = removeAllWhiteSpaces("5 * X^0 + 4 * X^1 - 9 * X^2 = 1 * X^0")
 
-    println(matchTest)
+    val splitAr = splitEquation(eqTest)
+
+    val coef: coefAndExp = splitAr.length match {
+      case 1 => parseCoef(splitAr.head)
+      case 2 => invertMap(parseCoef(splitAr(1))) combine parseCoef(splitAr.head)
+      case _ => throw new Exception("ERROR")
+    }
+
+    println(coef)
+
+//    if (stringSplit.length )
+
+//    println(parseCoefficients(eqTest))
 
 //    println("Type an equation ")
 //    "hello world".split(" ").foreach(println)
